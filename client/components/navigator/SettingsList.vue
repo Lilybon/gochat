dbbbe39695d6dd8e9d86715768f68590ac256add
Lilybon <zebra10029@gmail.com>
2020-08-28 17:23:46 +0800

    feat: complete settings list static template

--- client/components/navigator/SettingsList.vue
+++ client/components/navigator/SettingsList.vue
@@ -2,127 +2,184 @@
   <div>
     <v-list class="accent">
 
-      <v-list-item class="caption pr-0">
+      <v-list-item class="pr-0 mb-3" color="primary" :to="{ name: 'settings-edit-profile' }">
+        <v-list-item-avatar width="60" height="60">
+          <v-img :src="user.avatar"></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content class="d-flex no-wrap">
+          <v-list-item-title class="info--text font-weight-bold" v-text="user.name"></v-list-item-title>
+          <v-list-item-subtitle class="caption font-weight-medium info--text text--darken-1" v-text="user.phone"></v-list-item-subtitle>
+          <v-list-item-subtitle class="caption font-weight-medium info--text text--darken-1" v-text="user.tag"></v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-icon class="my-5">
+          <v-icon color="info darken-2" dense>{{ mdiChevronRight }}</v-icon>
+        </v-list-item-icon>
+      </v-list-item>
+
+      <v-list-item class="pr-0" color="primary" :to="{ name: 'settings-add-account' }">
         <v-list-item-icon class="ml-1 mr-6">
           <v-icon color="primary" dense>{{ mdiAccountPlusOutline }}</v-icon>
         </v-list-item-icon>
         <v-list-item-content>
-          <v-list-item-title class="primary--text" v-text="'Add contact'"></v-list-item-title>
+          <v-list-item-title class="primary--text" v-text="'Add Account'"></v-list-item-title>
         </v-list-item-content>
       </v-list-item>
-      <v-divider class="border--accent-lighten-1 mb-6" inset></v-divider>
+      <v-divider :class="['mb-6', `border--${ $route.name === 'settings-add-account' ? 'transparent' : 'accent-lighten-1' }`]" inset></v-divider>
+
       <template v-for="link in links">
         <v-list-item
           :key="`list-item-${ link.name }`"
           class="caption pr-0"
+          color="primary"
           :to="{ name: link.route }"
           nuxt
           dense
         >
-          <v-list-item-avatar width="25" height="25" min-width="25" tile>
-            <v-img src="https://en.meming.world/images/en/thumb/e/e2/Crying_Cat_with_paw_up.jpg/300px-Crying_Cat_with_paw_up.jpg"></v-img>
+          <v-list-item-avatar width="25" height="25" min-width="25" tile :class="['border--radius-sm', link.background]">
+            <v-icon small :color="link.iconColor">{{ link.icon }}</v-icon>
           </v-list-item-avatar>
           <v-list-item-content>
-            <v-list-item-title class="info--text" v-text="link.name"></v-list-item-title>
+            <v-list-item-title class="info--text" v-html="link.name"></v-list-item-title>
           </v-list-item-content>
+          <v-list-item-icon>
+            <v-icon color="info darken-2" dense>{{ mdiChevronRight }}</v-icon>
+          </v-list-item-icon>
         </v-list-item>
-        <v-divider inset :key="`divider-${ link.name }`"></v-divider>
+        <v-divider :class="`border--${ $route.name === link.route ? 'transparent' : 'accent-lighten-1' }`" inset :key="`divider-${ link.name }`"></v-divider>
       </template>
+
       <div class="mt-8"></div>
+
       <template v-for="help in helps">
         <v-list-item
           :key="`list-item-${ help.name }`"
-          class="caption pr-0"
+          class="pr-0"
           dense
         >
-          <v-list-item-avatar width="25" height="25" min-width="25" tile>
-            <v-img src="https://en.meming.world/images/en/thumb/e/e2/Crying_Cat_with_paw_up.jpg/300px-Crying_Cat_with_paw_up.jpg"></v-img>
+          <v-list-item-avatar width="25" height="25" min-width="25" tile :class="['border--radius-sm', help.background]">
+            <v-icon small :color="help.iconColor">{{ help.icon }}</v-icon>
           </v-list-item-avatar>
           <v-list-item-content>
-            <v-list-item-title class="info--text" v-text="help.name"></v-list-item-title>
+            <v-list-item-title class="info--text" v-html="help.name"></v-list-item-title>
           </v-list-item-content>
+          <v-list-item-icon>
+            <v-icon color="info darken-2" dense>{{ mdiChevronRight }}</v-icon>
+          </v-list-item-icon>
         </v-list-item>
-        <v-divider inset :key="`divider-${ help.name }`"></v-divider>
+        <v-divider class="border--accent-lighten-1" inset :key="`divider-${ help.name }`"></v-divider>
       </template>
+
       <div class="mt-8"></div>
+
     </v-list>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { mdiAccountPlusOutline } from '@mdi/js'
+import {
+  mdiAccountPlusOutline,
+  mdiChevronRight,
+  mdiCog,
+  mdiLock,
+  mdiDatabase,
+  mdiLaptop,
+  mdiFountainPen,
+  mdiWeb,
+  mdiStickerOutline,
+  mdiFolder,
+  mdiHelpCircle,
+  mdiDotsHorizontalCircle
+} from '@mdi/js'
 export default Vue.extend({
   name: 'SettingsList',
   data () {
     return {
       mdiAccountPlusOutline,
+      mdiChevronRight,
+      user: {
+        avatar: 'https://cdn2.ettoday.net/images/404/d404061.jpg',
+        name: 'Jason Chang',
+        phone: '+886 XXX XXX XXX',
+        tag: '@Lilybon'
+      },
       links: [
         {
-          icon: null,
+          icon: mdiCog,
+          iconColor: 'primary darken-2',
           name: 'General',
           route: 'settings-general-settings',
-          background: ''
+          background: 'info'
         },
         {
           icon: null,
+          iconColor: 'info',
           name: 'Notification and Sounds',
           route: 'settings-notifications',
-          background: ''
+          background: 'error'
         },
         {
-          icon: null,
+          icon: mdiLock,
+          iconColor: 'info',
           name: 'Privacy and Security',
           route: 'settings-privacy-and-security',
-          background: ''
+          background: 'primary darken-1'
         },
         {
-          icon: null,
+          icon: mdiDatabase,
+          iconColor: 'info',
           name: 'Data and Storage',
           route: 'settings-data-and-storage',
-          background: ''
+          background: 'success'
         },
         {
-          icon: null,
+          icon: mdiLaptop,
+          iconColor: 'info',
           name: 'Active Sessions',
           route: 'settings-active-sessions',
-          background: ''
+          background: 'warning'
         },
         {
-          icon: null,
+          icon: mdiFountainPen,
+          iconColor: 'info',
           name: 'Appearance',
           route: 'settings-appearance',
-          background: ''
+          background: 'primary lighten-1'
         },
         {
-          icon: null,
+          icon: mdiWeb,
+          iconColor: 'info',
           name: 'Language',
           route: 'settings-language',
-          background: ''
+          background: 'secondary'
         },
         {
-          icon: null,
+          icon: mdiStickerOutline,
+          iconColor: 'info',
           name: 'Stickers',
           route: 'settings-stickers',
-          background: ''
+          background: 'warning'
         },
         {
-          icon: null,
+          icon: mdiFolder,
+          iconColor: 'info',
           name: 'Chat Folders',
           route: 'settings-chat-folders',
-          background: ''
+          background: 'secondary'
         },
       ],
       helps: [
         {
-          icon: null,
+          icon: mdiHelpCircle,
+          iconColor: 'info',
           name: 'GoChat FAQ',
-          background: ''
+          background: 'primary lighten-1'
         },
         {
-          icon: null,
+          icon: mdiDotsHorizontalCircle,
+          iconColor: 'info',
           name: 'Ask a Question',
-          background: ''
+          background: 'info darken-2'
         }
       ]
     }
---
+++ client/pages/settings/add-account.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Add Account</div>
+</template>
+
+<script>
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'AddAccount'
+})
+</script>
